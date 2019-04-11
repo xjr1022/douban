4c9ac8eb9516dbd68b0ddc85b9423945377e025b
xjr1022 <xjr1022@gmail.com>
2019-04-11 22:28:31 +0800

    update

---
+++ component/list/list.js
@@ -0,0 +1,26 @@
+// component/list.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+      myPro:{
+        type:String,
+        value:'mypro',
+      }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      data1:"这里是data"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
